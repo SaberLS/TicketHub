a660d6a0879d80e3fc4734bb0cd5f1f82e733328
SaberLS <patryk.platek19@gmail.com>
2026-06-06 17:08:05 +0200

    add event entity

---
+++ tickethub/src/main/java/pl/wszib/java/advanced/tickethub/entity/Event.java
@@ -0,0 +1,30 @@
+package pl.wszib.java.advanced.tickethub.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String title;
+
+  private String category;
+
+  private String location;
+}
