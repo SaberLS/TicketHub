2c677bf7a36204a83ec11c02a7d1d5aaf3f00e36
SaberLS <patryk.platek19@gmail.com>
2026-06-06 16:52:46 +0200

    start spring in main

---
+++ tickethub/src/main/java/pl/wszib/java/advanced/tickethub/TicketHubApplication.java
@@ -0,0 +1,12 @@
+package pl.wszib.java.advanced.tickethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketHubApplication.class, args);
+    }
+}
